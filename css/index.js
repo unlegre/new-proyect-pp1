03ee61dbc5c7518a1a62c561c6fa2cc6c9d67771
“gabriel <unlegre@gmail.com>
2022-10-10 10:49:07 -0300

    ultimos cambios

--- css/index.js
+++ css/index.js
@@ -1,3 +1,14 @@
 function openWhatsApp(){
     window.open("https://api.whatsapp.com/send/?phone=541132871430&text=Hola gracias por comunicarte con Beautiful Woman");
 }
+const $btnSignIn=document.querySelector('.sign-in-btn'),
+      $btnSignUp=document.querySelector('.sign-up-btn'),  
+      $signUp=document.querySelector('.sign-up'),
+      $signIn=document.querySelector('.sign-in');
+document.addEventListener('click',e=>{
+    if(e.target===$btnSignIn || e.target===$btnSignUp ){
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+
+})
